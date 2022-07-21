a5d6d88f19183ba0a85639bde97979d70a1c80c1
Jacob <jakeywaldrip@gmail.com>
2022-07-20 21:34:43 -0500

    moved navbar into component

--- pomodo/src/App/App.jsx
+++ pomodo/src/App/App.jsx
@@ -1,48 +1,13 @@
 import 'bulma';
 
+import { Navbar } from '../Navbar/Navbar';
 import React from 'react';
-import { toggleNavbarBurger } from './bulma.js';
 
 function App(props) {
 
     return (
         <div className='container'>
-            <nav className="navbar" role="navigation" aria-label="main navigation">
-                <div className="navbar-brand">
-                    <a className="navbar-item" href="/">
-                        Pomogo
-                        {/* img here */}
-                    </a>
-
-                    { /* hamburger menu button */}
-                    <button className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarExpand" onClick={toggleNavbarBurger}>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </button>
-                </div>
-
-                <div id="navbarExpand" className="navbar-menu">
-                    <div className="navbar-start">
-                    <a href="/" className="navbar-item">
-                        Home
-                    </a>
-                </div>
-
-                    <div className="navbar-end">
-                    <div className="navbar-item">
-                        <div className="buttons">
-                        <button className="button is-primary">
-                            <strong>Sign up</strong>
-                        </button>
-                        <button className="button is-light">
-                            Log in
-                        </button>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-            </nav>
+            <Navbar />
         </div>
     );
 }
