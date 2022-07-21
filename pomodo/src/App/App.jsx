e09db6c50eb973cc6a827d31fa38c6b77d6f09d7
Jacob <jakeywaldrip@gmail.com>
2022-07-20 23:00:31 -0500

    added react router

--- pomodo/src/App/App.jsx
+++ pomodo/src/App/App.jsx
@@ -1,5 +1,12 @@
 import 'bulma';
 
+import {
+    BrowserRouter,
+    Route,
+    Routes,
+} from 'react-router-dom';
+
+import { Home } from '../Home/Home';
 import { Navbar } from '../Navbar/Navbar';
 import React from 'react';
 
@@ -8,6 +15,12 @@ function App(props) {
     return (
         <div className='container'>
             <Navbar />
+
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+        </BrowserRouter>
         </div>
     );
 }
---
+++ pomodo/src/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Home(props) {
+    return (
+        <>
+            <h1>Welcome to the home page!</h1>
+        </>
+    )
+}
